17957f9a11144545898db68bd26ef28d684b0db8
ivansantiagojr <ivansantiago.junior@gmail.com>
2025-11-02 00:25:36 -0300

    style: fix formatting

--- lua/jyba/init.lua
+++ lua/jyba/init.lua
@@ -6,137 +6,137 @@ local win_info = nil
 local data = { projects = {} }
 
 local function read_config_file(json_config_file_path)
-	local file = io.open(json_config_file_path, 'r')
-	local existing_data = {}
-	if file then
-		local content = file:read('*all')
-		file:close()
+    local file = io.open(json_config_file_path, 'r')
+    local existing_data = {}
+    if file then
+        local content = file:read('*all')
+        file:close()
 
-		existing_data = vim.fn.json_decode(content)
-		return existing_data
-	end
+        existing_data = vim.fn.json_decode(content)
+        return existing_data
+    end
 
-	return existing_data
+    return existing_data
 end
 
 M.run_cmd_on_save = function()
-	local project_name = vim.fn.getcwd()
-	local file_path = vim.fn.stdpath('data') .. '/jyba.json'
+    local project_name = vim.fn.getcwd()
+    local file_path = vim.fn.stdpath('data') .. '/jyba.json'
 
-	local existing_data = read_config_file(file_path)
+    local existing_data = read_config_file(file_path)
 
-	if existing_data.projects and existing_data.projects[project_name] then
-		local saved_commands = existing_data.projects[project_name].run_on_save
-		for _, command in pairs(saved_commands) do
-			vim.fn.jobstart(command, {
-				on_exit = function(_, code, _)
-					if code == 0 then
-						vim.cmd('e!')
-					end
-				end,
-			})
-		end
-	end
+    if existing_data.projects and existing_data.projects[project_name] then
+        local saved_commands = existing_data.projects[project_name].run_on_save
+        for _, command in pairs(saved_commands) do
+            vim.fn.jobstart(command, {
+                on_exit = function(_, code, _)
+                    if code == 0 then
+                        vim.cmd('e!')
+                    end
+                end,
+            })
+        end
+    end
 end
 
 api.nvim_create_autocmd("BufWritePost", {
-	group = api.nvim_create_augroup("Jyba", { clear = true }),
-	callback = function()
-		M.run_cmd_on_save()
-	end,
+    group = api.nvim_create_augroup("Jyba", { clear = true }),
+    callback = function()
+        M.run_cmd_on_save()
+    end,
 }
 )
 
 M.create_window = function()
-	if win_info then return end
+    if win_info then return end
 
-	local width = 60
-	local height = 10
-	local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+    local width = 60
+    local height = 10
+    local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
 
-	local bufnr = vim.api.nvim_create_buf(false, false)
+    local bufnr = vim.api.nvim_create_buf(false, false)
 
-	local win_id, win = popup.create(bufnr, {
-		title = "Jyba",
-		highlight = "JybaWindow",
-		line = math.floor(((vim.o.lines - height) / 2) - 1),
-		col = math.floor((vim.o.columns - width) / 2),
-		minwidth = width,
-		minheight = height,
-		borderchars = borderchars,
-	})
+    local win_id, win = popup.create(bufnr, {
+        title = "Jyba",
+        highlight = "JybaWindow",
+        line = math.floor(((vim.o.lines - height) / 2) - 1),
+        col = math.floor((vim.o.columns - width) / 2),
+        minwidth = width,
+        minheight = height,
+        borderchars = borderchars,
+    })
 
-	vim.wo[win_id].number = true
+    vim.wo[win_id].number = true
 
-	win_info = {
-		bufnr = bufnr,
-		win_id = win_id,
-	}
+    win_info = {
+        bufnr = bufnr,
+        win_id = win_id,
+    }
 
-	local project_name = os.getenv('PWD')
-	local file_path = vim.fn.stdpath('data') .. '/jyba.json'
+    local project_name = os.getenv('PWD')
+    local file_path = vim.fn.stdpath('data') .. '/jyba.json'
 
-	local existing_data = read_config_file(file_path)
+    local existing_data = read_config_file(file_path)
 
-	if existing_data.projects and existing_data.projects[project_name] then
-		local saved_commands = existing_data.projects[project_name].run_on_save
-		local commands = {}
+    if existing_data.projects and existing_data.projects[project_name] then
+        local saved_commands = existing_data.projects[project_name].run_on_save
+        local commands = {}
 
-		-- Add commands to new window
-		for i, command in pairs(saved_commands) do
-			commands[i] = command
-		end
-		api.nvim_buf_set_lines(bufnr, 0, -1, false, commands)
-	end
+        -- Add commands to new window
+        for i, command in pairs(saved_commands) do
+            commands[i] = command
+        end
+        api.nvim_buf_set_lines(bufnr, 0, -1, false, commands)
+    end
 end
 
 
 M.write_to_json = function(lines)
-	local project_name = os.getenv('PWD')
-	local file_path = vim.fn.stdpath('data') .. '/jyba.json'
+    local project_name = os.getenv('PWD')
+    local file_path = vim.fn.stdpath('data') .. '/jyba.json'
 
-	local file = io.open(file_path, 'r')
-	data = read_config_file(file_path)
+    local file = io.open(file_path, 'r')
+    data = read_config_file(file_path)
 
-	-- Append the new command to the existing ones
-	if data.projects then
-		if data.projects[project_name] then
-			data.projects[project_name].run_on_save = lines
-		else
-			data.projects[project_name] = { run_on_save = lines }
-		end
-	else
-		data.projects = { [project_name] = { run_on_save = lines } }
-	end
+    -- Append the new command to the existing ones
+    if data.projects then
+        if data.projects[project_name] then
+            data.projects[project_name].run_on_save = lines
+        else
+            data.projects[project_name] = { run_on_save = lines }
+        end
+    else
+        data.projects = { [project_name] = { run_on_save = lines } }
+    end
 
-	local json_str = vim.fn.json_encode(data)
+    local json_str = vim.fn.json_encode(data)
 
-	file = io.open(file_path, 'w')
-	if file then
-		file:write(json_str)
-		file:close()
-	else
-		print('Failed to open file')
-	end
+    file = io.open(file_path, 'w')
+    if file then
+        file:write(json_str)
+        file:close()
+    else
+        print('Failed to open file')
+    end
 end
 
 M.destroy_window = function()
-	if win_info then
-		local opts = { force = true }
-		api.nvim_buf_delete(win_info.bufnr, opts)
-		win_info = nil
-	end
+    if win_info then
+        local opts = { force = true }
+        api.nvim_buf_delete(win_info.bufnr, opts)
+        win_info = nil
+    end
 end
 
 M.toggle_window = function()
-	if win_info then
-		local bufnr = win_info.bufnr
-		local lines = api.nvim_buf_get_lines(bufnr, 0, -1, false)
-		M.write_to_json(lines)
-		M.destroy_window()
-	else
-		M.create_window()
-	end
+    if win_info then
+        local bufnr = win_info.bufnr
+        local lines = api.nvim_buf_get_lines(bufnr, 0, -1, false)
+        M.write_to_json(lines)
+        M.destroy_window()
+    else
+        M.create_window()
+    end
 end
 
 
